e7b264f0e5b4977a788b22a83cb7b06766cfeb82
AdityaSaiKonduri <adityakonduri8@gmail.com>
2024-06-24 00:04:43 +0530

    Navbar and Auth

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './_components/Navbar/Navbar'
+import Auth from './_components/auth/auth'
+
+const page = () => {
+  return (
+    <main>
+      <Navbar />
+      <Auth />
+    </main>
+  )
+}
+
+export default page
